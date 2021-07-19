b94b00115714c9d5722158432a5889b06e863cfd
Artem Frolov <57588157+artemf92@users.noreply.github.com>
2021-07-19 19:36:29 +0200

    настроил слайдеры на главной странице

--- src/js/custom.js
+++ src/js/custom.js
@@ -32,3 +32,95 @@ let enableScroll = function () {
   window.scroll({ top: pagePosition, left: 0 });
   document.body.removeAttribute('data-position');
 }
+
+$(document).ready(function() {
+  $('.hero__features-item').matchHeight();
+
+  if ($('.hero__features').length) {
+    $(".hero__features").slick({
+      mobileFirst: true,
+      variableWidth: true,
+      infinite: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: false,
+      centerMode: true,
+      autoplay: true,
+      arrows: false,
+      responsive: [
+        {
+          breakpoint: 570,
+          settings: {
+            centerMode: false,
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            centerMode: false,
+          }
+        },
+        {
+          breakpoint: 992,
+          settings: "unslick",
+      },
+      ]
+    });
+  }
+  if ($('.teachers__item').length > 1) {
+    $('.teachers__arrows').css('display', 'flex')
+    $('.teachers__list').slick({
+      infinite: false,
+      prevArrow: $('.teachers__arrows--prev'),
+      nextArrow: $('.teachers__arrows--next')
+    })
+  }
+
+  if ($('.gallery__list').length) {
+    $('.gallery__list').slick({
+      slidesToShow: 5,
+      centerMode: true,
+      autoplay: true,
+      prevArrow: $('.gallery__arrows--prev'),
+      nextArrow: $('.gallery__arrows--next'),
+      responsive: [
+        {
+          breakpoint: 1600,
+          settings:{
+            slidesToShow: 4,
+          }
+        },
+        {
+          breakpoint:992,
+          settings:{
+            slidesToShow: 3,
+            centerMode: true,
+          }
+        },
+        {
+          breakpoint: 768,
+          settings:{
+            slidesToShow: 2,
+            centerMode: true,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings:{
+            slidesToShow: 1,
+            centerMode: true,
+          }
+        },
+      ]
+    })
+  }
+  if ($('.reviews__item').length > 1) {
+    $('.reviews__arrows').css('display', 'flex')
+    $('.reviews__list').slick({
+      infinite: false,
+      prevArrow: $('.reviews__arrows--prev'),
+      nextArrow: $('.reviews__arrows--next'),
+    })
+  }
+})
